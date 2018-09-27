b6cd8e7fcc51e652e1b7da83f582efda11dc1bd1
PERIN <perin011@pc02C.iut-rcc-info.urca>
2018-09-27 11:12:11 +0200

    helloworldV2

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,17 @@
+<?php
+// src/Controller/BlogController.php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController
+{
+    /**
+     * @Route("/blog/{page}", name="blog_list", requirements={"page": "\d+"})
+     */
+    function list($page = 1) {
+        return new Response('<html><body>blog  ' . $page . '</body></html>');
+    }
+}
---
+++ src/Controller/HelloWorldController.php
@@ -0,0 +1,21 @@
+<?php
+// src/Controller/HelloWorldController.php
+
+namespace App\Controller;
+
+//use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloWorldController extends Controller
+{
+    /**
+     * @Route("/hello/{name}", name="hello_name", requirements={"name": "[a-zA-Z]+"})
+     */
+    public function index($name = "World !")
+    {
+
+        //return new Response('<html><body>Hello ' . $name . '</body></html>');
+        return $this->render('helloWorld.html.twig', ['name' => $name]);
+    }
+}
