ea8736385b380209f11462d646b2aabde3156ec1
PERIN <perin011@pc02C.iut-rcc-info.urca>
2018-10-04 08:40:47 +0200

    search fini

--- src/Controller/QuoteController.php
+++ src/Controller/QuoteController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class QuoteController extends Controller
@@ -10,9 +11,20 @@ class QuoteController extends Controller
     /**
      * @Route("/quotes/", name="list_quotes")
      */
-    public function index()
+    public function index(Request $rq)
     {
-        $quotes = json_decode(file_get_contents("../var/quotes.json"));
+        $search = $rq->query->get('search');
+        $quotes = json_decode(file_get_contents("../var/quotes.json"), true);
+
+        if ($search != "") {
+            $tab = [];
+            foreach ($quotes as $q) {
+                if (strpos($q['quote'], $search) !== false) {
+                    array_push($tab, $q);
+                }
+            }
+            $quotes = $tab;
+        }
         return $this->render('quote.html.twig', ['quotes' => $quotes]);
     }
 }
